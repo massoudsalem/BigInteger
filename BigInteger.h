06297ee9c57d73105c85ea2feeda9084b9dfd905
Omar Ali Hassan <36759605+OmarAli3@users.noreply.github.com>
2019-01-28 21:04:38 +0200

    Updating h file&adding more function
    changing the function parameter to const referrence.
    adding setter and getter functions.
    adding overloading input and output functions.

--- BigInteger.h
+++ BigInteger.h
@@ -5,39 +5,48 @@ class BigInteger{
 
 public:
   BigInteger();                       //zero initialization (constructor)
-  BigInteger(string val);             //string number valute initialization +ve and -ve using first char in string (constructor)
-  BigInteger(string val,bool sign);   //string number valute initialization with sign (constructor)
+  BigInteger(string val,bool sign);   //string number value initialization with sign (constructor)
+  BigInteger(string val);             //string number value initialization +ve and -ve using first char in string (constructor)
   BigInteger(int val);                //integer (32bit) number value (constructor)
-  BigInteger(long long val);          //long (64bit) number valute (constructor)
+  BigInteger(long long val);          //long (64bit) number value (constructor)
+  
+  void SetVal(string val);            //string number value (setter)
+  void SetVal(int val);               //integer (32bit) number value (setter)
+  void SetVal(long long val);         //long (64bit) number value (setter)
+  void SetSign(bool sign);            //sign setter
+  string GetVal();                    //string value getter
+  bool GetSign();                     //sign getter
 
-  void operator = (BigInteger bigInteger);                //overloading for assignment operator
-  BigInteger operator + (BigInteger bigInteger);          //overloading for adding operator
-  BigInteger operator - (BigInteger bigInteger);          //overloading for subtracting operator
-  BigInteger operator * (BigInteger bigInteger);          //overloading for multiplying operator
-  BigInteger operator / (BigInteger bigInteger);          //overloading for dividing operator
-  BigInteger operator % (BigInteger bigInteger);          //overloading for remainder operator
+  void operator = (const BigInteger &bigInteger);                //overloading for assignment operator
+  BigInteger operator + (const BigInteger &bigInteger);          //overloading for adding operator
+  BigInteger operator - (const BigInteger &bigInteger);          //overloading for subtracting operator
+  BigInteger operator * (const BigInteger &bigInteger);          //overloading for multiplying operator
+  BigInteger operator / (const BigInteger &bigInteger);          //overloading for dividing operator
+  BigInteger operator % (const BigInteger &bigInteger);          //overloading for remainder operator
 
-  BigInteger& operator += (BigInteger bigInteger);        //overloading for adding compound operator
-  BigInteger& operator -= (BigInteger bigInteger);        //overloading for subtracting compound operator
-  BigInteger& operator *= (BigInteger bigInteger);        //overloading for multiplying compound operator
-  BigInteger& operator /= (BigInteger bigInteger);        //overloading for dividing compound operator
-  BigInteger& operator %= (BigInteger bigInteger);        //overloading for remainder compound operator
+  BigInteger& operator += (const BigInteger &bigInteger);        //overloading for adding compound operator
+  BigInteger& operator -= (const BigInteger &bigInteger);        //overloading for subtracting compound operator
+  BigInteger& operator *= (const BigInteger &bigInteger);        //overloading for multiplying compound operator
+  BigInteger& operator /= (const BigInteger &bigInteger);        //overloading for dividing compound operator
+  BigInteger& operator %= (const BigInteger &bigInteger);        //overloading for remainder compound operator
 
   BigInteger& operator ++ ();             //prefix increment: no parameter, returns a reference
   BigInteger& operator -- ();             //prefix decrement: no parameter, returns a reference
   BigInteger  operator ++ (int);          //postfix increment: dummy parameter, returns a value
   BigInteger  operator -- (int);          //postfix decrement: dummy parameter, returns a value
 
-  bool operator == (BigInteger bigInteger);               //overloading for equals comparison operator
-  bool operator != (BigInteger bigInteger);               //overloading for not equals comparison operator
-  bool operator >= (BigInteger bigInteger);               //overloading for greaterthan or equal comparison operator
-  bool operator <= (BigInteger bigInteger);               //overloading for lessthan or equal comparison operator
-  bool operator > (BigInteger bigInteger);                //overloading for greaterthan comparison operator
-  bool operator < (BigInteger bigInteger);                //overloading for lessthan comparison operator
+  bool operator == (const BigInteger &bigInteger);               //overloading for equals comparison operator
+  bool operator != (const BigInteger &bigInteger);               //overloading for not equals comparison operator
+  bool operator >= (const BigInteger &bigInteger);               //overloading for greaterthan or equal comparison operator
+  bool operator <= (const BigInteger &bigInteger);               //overloading for lessthan or equal comparison operator
+  bool operator > (const BigInteger &bigInteger);                //overloading for greaterthan comparison operator
+  bool operator < (const BigInteger &bigInteger);                //overloading for lessthan comparison operator
 
   BigInteger operator -();                 //-ve sign
   operator string();                       //string conversion
-
+  
+  friend ostream & operator << (ostream &out, const BigInteger &bigInteger);  //overloading for using cout
+  friend istream & operator >> (istream &in,  BigInteger &bigInteger);        //overloading for using cout
 
 private:
   string val;                         //string value
